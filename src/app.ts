2bc7d3c0ea7366803041861f75edcd84632da4a3
Brian Haack <haack79@hotmail.com>
2024-05-21 23:29:46 -0700

    updated app and config to be more encapsulated and allow for flexibility

--- src/app.ts
+++ src/app.ts
@@ -2,48 +2,18 @@ import express, { Express } from 'express';
 import Server from './server';
 import './dotenv';
 
-const app: Express = express();
-const server = new Server(app);
+class Application {
+    private server: Server
+    
+    constructor() {
+        const app: Express = express();
+        this.server = new Server(app);
+    }
+    public start(): void {
+        this.server.start();
+    }
 
-server.start();
+}
 
-// const express = require('express');
-// // set up app to use express to handle routes, middleware and other aspects of webserver. 
-// const app = express();
-// // body parser to parse incoming requests, does not handle multipart forms or file uploads or binary data
-// // but is enough for most use cases
-// const bodyParser = require('body-parser');
-// // get the routes for the app
-
-// // connect whichever database you want to use
-
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json());
-
-// // set up cors to allow cross origin requests
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//     if(req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//         return res.status(200).json({});
-//     }
-//     next();
-// });
-
-// // set up routes for the app
-
-
-// // handle missed requests to 404
-// app.use((req, res, next) => {
-//     const error = new Error('Page not found');
-//     error.status = 404;
-//     next(error);
-// })
-// // handle errors in the app
-// app.use((error, req, res, next) => {
-//     res.status(error.status || 500);
-//     res.json({
-//         message: error.message
-//     })
-// })
+const application = new Application();
+application.start();
---
+++ src/config.ts
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+class Config {
+    public MONGO_ATLAS_PW: string | undefined
+    public JWT_KEY: string | undefined
+
+    private readonly DATABASE_URL='mongodb+srv://haack79:' + process.env.MONGO_ATLAS_PW + '@cluster0.4qubeli.mongodb.net/'
+
+    constructor() {
+        this.MONGO_ATLAS_PW = process.env.MONGO_ATLAS_PW
+        this.JWT_KEY = process.env.JWT_KEY
+    }
+    public validateConfigOptions(): void {
+        if (!this.MONGO_ATLAS_PW) {
+            throw new Error('No mongo password set in env')
+        }
+        if (!this.JWT_KEY) {
+            throw new Error('No jwt key set in env')
+        }
+    }
+}
+export const config: Config = new Config()
